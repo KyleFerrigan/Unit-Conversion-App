c328e0859b8e80b071a1a243461f0ed88bbe1ce3
Kyle Ferrigan <kbferrigan@icloud.com>
2022-09-20 16:56:49 -0500

    Initial Commit

---
+++ Multi-Convert/Multi_ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multi_ConvertApp.swift
+//  Multi-Convert
+//
+//  Created by Kyle Ferrigan on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Multi_ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
