86f0684c20b831c22b82d65a537a87e778ae2de2
Kyle Ferrigan <kyle@ferrigan.dev>
2022-12-22 16:16:52 -0600

    Mac Catalyst App now shows correct window size

--- Multi-Convert/Multi_ConvertApp.swift
+++ Multi-Convert/Multi_ConvertApp.swift
@@ -12,6 +12,15 @@ struct Multi_ConvertApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+			.onReceive(NotificationCenter.default.publisher(for: UIScene.willConnectNotification)) { _ in
+				#if targetEnvironment(macCatalyst)
+				// prevent macOS window from being resized
+				  UIApplication.shared.connectedScenes.compactMap { $0 as? UIWindowScene }.forEach { windowScene in
+					windowScene.sizeRestrictions?.maximumSize = CGSize(width: 500, height: 500)
+					windowScene.sizeRestrictions?.minimumSize = CGSize(width: 500, height: 500)
+				}
+				#endif
+			}
+		}
     }
 }
